d480a41bc39662a8ca09d2fe3d75e7ddbf049a08
Greg Rozmarynowycz <crashtest1@live.com>
2016-09-25 17:22:53 -0400

    work on audio viz project

--- audioViz/services/visualizer.svc.js
+++ audioViz/services/visualizer.svc.js
@@ -9,7 +9,7 @@ app.constant('Effects', Object.freeze({
         LINES: 'LINES',
         DESATURATE: 'DESATURATE'
     }))
-    .service('Visualizer', function(Scheduler, AudioPlayerService, EaselService, SAMPLE_COUNT, Effects){
+    .service('Visualizer', function(Scheduler, AudioPlayerService, EaselService, SampleCount, Effects){
 
         // HELPER
         function makeColor(red, green, blue, alpha){
@@ -19,7 +19,8 @@ app.constant('Effects', Object.freeze({
         //The average of the frequency values from the last frame
         var lastFrameAvg = 0;
         //The maximum range of values with valid data present in the clip
-        var dataLimit = 0;
+        var dataLimit = 0,
+            angle = 0;
 
         var visualizer = {
             init(){
@@ -36,14 +37,6 @@ app.constant('Effects', Object.freeze({
             brightness: 1,
             effects: []
         };
-
-        // function applyActiveEffects(, x, y){
-        //
-        // }
-
-        function isActiveEffect(effect){
-            return visualizer.effects.indexOf(effect) > -1;
-        }
 
         function getActiveEffects() {
             var effects = {};
@@ -103,6 +96,84 @@ app.constant('Effects', Object.freeze({
             ctx.putImageData(imgData, 0, 0);
         }
 
+        function getDataLimit(data, start){
+            var limit = start || 0;
+            for(var i = start; i < data.length; i++){
+                if(data[i] > 0 && i > limit){
+                    limit = i;
+                }
+            }
+            return limit - limit % 2;
+        }
+
+        function drawArcs(ctx, data){
+            var canvas = ctx.canvas,
+                origin = {x: canvas.width / 2, y: canvas.height / 2};
+
+            //keep track of how many indices in the data array actually have values
+            //This prevents a large slice of the visualizer from being empty early in the song or for songs that smaller range of data
+            dataLimit = getDataLimit(data, dataLimit);
+
+            //The length of each arc
+            var arcLength = (4 * Math.PI) / dataLimit;
+
+            var frameAvg = 0;
+            ctx.fillStyle = "#55aaff";
+            ctx.strokeStyle = "#fc0";
+            ctx.beginPath();
+            // loop through the data and draw!
+            for (var i = 0; i < dataLimit / 2; i++) {
+
+                if(data[i] > 0){
+                    ctx.moveTo(origin.x, origin.y);
+                    //Fudge the radius of the arcs based on the overall average of the previous range to the whole set of arcs is fuller
+                    ctx.arc(origin.x, origin.y, (canvas.width / 2) * ((data[i] + lastFrameAvg) / 512), angle + i * arcLength, angle + (i + 1) * arcLength);
+                    ctx.closePath();
+                }
+
+                //Add the current value to the average
+                frameAvg += data[i] / dataLimit;
+            }
+
+            ctx.fill();
+            ctx.stroke();
+
+            ctx.fillStyle = "rgba(100, 50, 200, .5)";
+            ctx.strokeStyle = "rgba(0, 0, 50, .5)";
+            ctx.beginPath();
+
+            for (i; i < dataLimit; i++) {
+
+                if(data[i] > 0){
+                    ctx.moveTo(origin.x, origin.y);
+                    //Fudge the radius of the arcs based on the overall average of the previous range to the whole set of arcs is fuller
+                    ctx.arc(origin.x, origin.y, (canvas.width / 2) * ((data[i] + lastFrameAvg) / 512), -angle + i * arcLength, -angle + (i + 1) * arcLength);
+                    ctx.closePath();
+                }
+
+                //Add the current value to the average
+                frameAvg += data[i] / dataLimit;
+            }
+
+            ctx.fill();
+            ctx.stroke();
+
+            var gradient = ctx.createRadialGradient(origin.x, origin.y, frameAvg / 10, origin.x, origin.y, frameAvg);
+            gradient.addColorStop(0, "#000");
+            gradient.addColorStop(1, "rgba(0,0,0,0)");
+
+            ctx.fillStyle = gradient;
+            ctx.beginPath();
+            ctx.moveTo(origin.x, origin.y);
+            ctx.arc(origin.x, origin.y, frameAvg, 0, 2 * Math.PI);
+            ctx.fill();
+
+            angle += frameAvg / 10000;
+
+            //store the average for the next frame
+            lastFrameAvg = frameAvg;
+        }
+
         function update() {
             var analyzerNode = AudioPlayerService.getAnalyzerNode();
 
@@ -110,76 +181,17 @@ app.constant('Effects', Object.freeze({
                 return;
             }
 
-            var data = new Uint8Array(SAMPLE_COUNT / 2);
+            var data = new Uint8Array(SampleCount / 2);
             // populate the array with the frequency data
             analyzerNode.getByteFrequencyData(data);
 
             // OR
-            //analyserNode.getByteTimeDomainData(data); // waveform data
+            //analyzerNode.getByteTimeDomainData(data); // waveform data
 
-            Scheduler.draw(()=> {
-                var ctx = EaselService.context,
-                    canvas = ctx.canvas,
-                    origin = {x: canvas.width / 2, y: canvas.height / 2};
+            //Draw visualization
+            Scheduler.draw(()=> drawArcs(EaselService.context, data), 100);
 
-                //The length of each arc
-                var arcLength = (2 * Math.PI) / dataLimit;
-
-                var frameAvg = 0;
-                // loop through the data and draw!
-                for (var i = 0; i < data.length; i++) {
-
-                    if(data[i] > 0){
-                        ctx.beginPath();
-                        ctx.fillStyle = "#55aaff";
-                        ctx.moveTo(origin.x, origin.y);
-                        //Fudge the radius of the arcs based on the overall average of the previous range to the whole set of arcs is fuller
-                        ctx.arc(origin.x, origin.y, (canvas.width / 2) * ((data[i] + lastFrameAvg) / 512), i * arcLength, (i + 1) * arcLength);
-                        ctx.closePath();
-                        ctx.fill();
-
-                        //keep track of how many indices in the data array actually have values
-                        //This prevents a large slice of the visualizer from being empty early in the song or for songs that smaller range of data
-                        if(i > dataLimit){
-                            dataLimit = i;
-                        }
-                    }
-
-                    //red-ishcircles
-                    var percent = data[i] / 255;
-                    var maxRadius = visualizer.maxRadius;
-                    var circleRadius = percent * maxRadius;
-
-                    ctx.beginPath();
-                    ctx.fillStyle = makeColor(255, 111, 111, .34 - percent / 3.0);
-                    ctx.arc(origin.x, origin.y, circleRadius, 0, 2 * Math.PI, false);
-                    ctx.fill();
-                    ctx.closePath();
-
-                    //blue-ishcircles,bigger,moretransparent
-                    ctx.beginPath();
-                    ctx.fillStyle = makeColor(0, 0, 255, .10 - percent / 10.0);
-                    ctx.arc(origin.x, origin.y, circleRadius * 1.5, 0, 2 * Math.PI, false);
-                    ctx.fill();
-                    ctx.closePath();
-
-                    //yellow-ishcircles,smaller
-                    ctx.save();
-                    ctx.beginPath();
-                    ctx.fillStyle = makeColor(200, 200, 0, .5 - percent / 5.0);
-                    ctx.arc(origin.x, origin.y, circleRadius * .50, 0, 2 * Math.PI, false);
-                    ctx.fill();
-                    ctx.closePath();
-                    ctx.restore();
-
-                    //Add the current value to the average
-                    frameAvg += data[i] / data.length;
-                }
-
-                //store the average for the next frame
-                lastFrameAvg = frameAvg;
-            }, 100);
-
+            //Apply pixel manipulation effects
             Scheduler.postProcess(()=>manipulatePixels(EaselService.context));
         }
 
