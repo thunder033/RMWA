cde9b512f128430447ef81104606b81355e7678e
Greg <gjr8050@rit.edu>
2016-10-24 16:17:56 -0400

    at ui router states and being warp

--- audioViz/pulsar.js
+++ audioViz/pulsar.js
@@ -2,7 +2,18 @@
  * Created by gjr8050 on 9/14/2016.
  */
 "use strict";
-var app = angular.module('pulsar', ['mallet','checklist-model'])
+var app = angular.module('pulsar', ['mallet','checklist-model', 'ui.router'])
+    .config(function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/visualizer');
+
+        $stateProvider.state('visualizer', {
+            url: '/visualizer',
+            template: '<control-panel></control-panel><m-easel id="visualizer"></m-easel>'
+        }).state('warp', {
+            url: '/warp',
+            template: 'This is Warp'
+        });
+    })
     .run(function(MScheduler, Visualizer){
         Visualizer.init();
         MScheduler.startMainLoop();
