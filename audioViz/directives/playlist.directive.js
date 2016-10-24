dd384c87c5fc5f7584520878690b8fd61e327ce3
Greg <gjr8050@rit.edu>
2016-10-24 16:08:55 -0400

    fix song

--- audioViz/directives/playlist.directive.js
+++ audioViz/directives/playlist.directive.js
@@ -2,13 +2,13 @@
  * Created by Greg on 9/18/2016.
  */
 "use strict";
-app.directive('playlist', function(AudioClipService, AudioPlayerService){
+app.directive('playlist', function(AudioClipService, AudioPlayerService, MediaType){
     return {
         restrict: 'E',
         replace: true,
         templateUrl: 'templates/playlist.html',
         link: function(scope){
-            scope.clips = AudioClipService.getAudioClips('media');
+            scope.clips = AudioClipService.getAudioClips(MediaType.Clip);
 
             scope.playClip = function(clipId) {
                 AudioPlayerService.playClip(clipId);
