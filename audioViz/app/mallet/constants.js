a7783c12fe38d3e8a1326199ec80b4b18c1ada80
Greg Rozmarynowycz <crashtest1@live.com>
2016-10-28 21:35:41 -0400

    add warp ship and do some refactoring

--- audioViz/app/mallet/constants.js
+++ audioViz/app/mallet/constants.js
@@ -2,4 +2,17 @@ angular.module('mallet-constants', [])
     //Rendering
     .constant('ScaleFactor', (()=>window.devicePixelRatio || 1)())
     .constant('SampleCount', 1024)
-    .constant('MaxFrameRate', 60);
+    .constant('MaxFrameRate', 60)
+    .constant('MKeys', Object.freeze({
+        Down: 40,
+        Up: 38,
+        Right: 39,
+        Left: 37,
+        Space: 32,
+        Escape: 27
+    }))
+    .constant('MState', Object.freeze({
+        Playing: 'Playing',
+        Paused: 'Paused',
+        Loading: 'Loading'
+    }));
---
+++ audioViz/app/mallet/keys.svc.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Greg on 10/28/2016.
+ */
+"use strict";
+angular.module('mallet').service('MKeyboard', [function(){
+    var keyDownEvents = [],
+        keyUpEvents = [];
+
+    function invokeListeners(listeners, e){
+        listeners.forEach(listener => {
+            //this is sort of unreliable but should be good enough for our purposes
+            if(listener.key == e.keyCode || listener.key === String.fromCharCode(e.keyCode)){
+                listener.callback(e);
+            }
+        });
+    }
+
+    window.addEventListener('keyup', e => {
+        invokeListeners(keyUpEvents, e);
+    });
+    
+    window.addEventListener('keydown', e => {
+        invokeListeners(keyDownEvents, e);
+    });
+
+    this.onkeydown = (key, callback) => {
+        keyDownEvents.push({key: key, callback: callback});
+    };
+
+    this.onkeyup = (key, callback) => {
+        keyUpEvents.push({key: key, callback: callback});
+    };
+}]);
