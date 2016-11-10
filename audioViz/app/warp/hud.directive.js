db0d0f3fd4a62643c57b660ca6a8c4eb59441b7c
Greg <gjr8050@rit.edu>
2016-11-10 12:12:32 -0500

    mover warp.ship to own file

--- audioViz/app/warp/hud.directive.js
+++ audioViz/app/warp/hud.directive.js
@@ -2,14 +2,14 @@
  * Created by Greg on 10/29/2016.
  */
 "use strict";
-angular.module('pulsar-warp').directive('warpHud', ['WarpState','MScheduler', 'AudioPlayerService', 'WarpShip', 'Warp', function(WarpState, MScheduler, AudioPlayer, WarpShip, Warp){
+angular.module('pulsar-warp').directive('warpHud', ['WarpState','MScheduler', 'AudioPlayerService', 'warp.ship', 'Warp', function(WarpState, MScheduler, AudioPlayer, ship, Warp){
     return {
         restrict: 'E',
         templateUrl: 'views/warp-hud.html',
         link: function(scope){
             scope.warpState = WarpState;
             scope.player = AudioPlayer;
-            scope.warp = WarpShip;
+            scope.warp = ship;
 
             scope.resume = () => {
                 MScheduler.resume();
