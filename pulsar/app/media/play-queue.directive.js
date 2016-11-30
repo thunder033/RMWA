3603de77cc38f222e81096e1dc9087a36b636e80
Gregory Rozmarynowycz (RIT Student) <gjr8050@ad.rit.edu>
2016-11-30 11:52:29 -0500

    start playqueue and media widget

---
+++ pulsar/app/media/play-queue.directive.js
@@ -0,0 +1,25 @@
+/**
+ * Created by gjr8050 on 11/30/2016.
+ */
+'use strict';
+require('angular')
+    .module('pulsar.media')
+    .directive('playQueue', [
+        'audio.Player',
+        playQueueDirective
+    ]);
+
+function playQueueDirective(AudioPlayer){
+    return {
+        restrict: 'E',
+        link: function(scope){
+            /**
+             *
+             * @type {Array<IPlayable>}
+             */
+            scope.queue = [];
+
+            AudioPlayer.
+        }
+    }
+}
