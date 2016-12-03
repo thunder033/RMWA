5fa5e849b176bf2c0c8a07c1efbd55c71aaab217
Greg Rozmarynowycz <crashtest1@live.com>
2016-12-02 20:18:31 -0500

    flare mute button, improvements to audio player styling

--- pulsar/app/flare/audio-player.directive.js
+++ pulsar/app/flare/audio-player.directive.js
@@ -2,7 +2,7 @@
  * Created by gjr8050 on 9/16/2016.
  */
 'use strict';
-require('angular').module('pulsar.flare').directive('audioPlayer', ['audio.Player', function(AudioPlayer){
+require('angular').module('pulsar.flare').directive('audioPlayer', ['audio.Player', '$sce', '$timeout', function(AudioPlayer, $sce, $timeout){
     return {
         restrict: 'E',
         replace: true,
@@ -12,6 +12,22 @@ require('angular').module('pulsar.flare').directive('audioPlayer', ['audio.Playe
             scope.getPlaybarSize = function(){
                 return AudioPlayer.completionPct * 100 + '%';
             };
+
+            scope.toTrusted = function(html){
+                return $sce.trustAsHtml(html);
+            };
+
+            scope.toggleMute = () => {
+                scope.muted = !scope.muted;
+                localStorage.setItem('pulsar-muted', scope.muted === true ? '1' : '0');
+                AudioPlayer.setOutputGain(scope.muted ? 0 : 1);
+            };
+            var cachedMute = parseInt(localStorage.getItem('pulsar-muted') || '0');
+            scope.muted = cachedMute === 1;
+
+            if(scope.muted){
+                $timeout(()=>  AudioPlayer.setOutputGain(0));
+            }
             
             function getMouse(e){
                 var mouse = {}; // make an object
