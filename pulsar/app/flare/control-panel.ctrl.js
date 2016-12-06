f54ae3008a970a65328f8152881362e33d04ea3a
Greg Rozmarynowycz <crashtest1@live.com>
2016-12-06 00:03:00 -0500

    add notification to play queue for added songs

--- pulsar/app/flare/control-panel.ctrl.js
+++ pulsar/app/flare/control-panel.ctrl.js
@@ -6,15 +6,43 @@ require('angular').module('pulsar.flare').controller('ControlPanelCtrl', [
     '$scope',
     'media.Library',
     'audio.Player',
-    'media.const.State',
     'media.PlayQueue',
+    '$timeout',
     ControlPanelCtrl]);
 
-function ControlPanelCtrl($scope, MediaLibrary, AudioPlayer, MediaState, PlayQueue){
+function ControlPanelCtrl($scope, MediaLibrary, AudioPlayer, PlayQueue, $timeout){
+
     MediaLibrary.isReady().then(()=>{
         AudioPlayer.stop();
     });
 
+    $scope.playQueueAdded = ''; //The title of the last added song
+    $scope.notifcationEvent = null; //Handle to the timeout event (hiding the notification)
+    
     $scope.player = AudioPlayer;
     $scope.playQueue = new PlayQueue(AudioPlayer);
+
+    $scope.isActiveNotification = function(){
+        return $scope.playQueueAdded !== '' && $scope.playQueueAdded !== null;
+    };
+
+    $scope.playQueue.addEventListener('itemAdded', e =>{
+        if(!e.item){
+            return;
+        }
+
+        //Get the name of added item
+        $scope.playQueueAdded = e.item.getName();
+
+        // cancel out the previous notification hide event
+        if($scope.notifcationEvent !== null){
+            $timeout.cancel($scope.notifcationEvent);
+        }
+
+        //Make the notification disappear after a delay
+        $scope.notifcationEvent = $timeout(()=>{
+            $scope.playQueueAdded = null;
+            $scope.notifcationEvent = null;
+        }, 1500);
+    });
 }
